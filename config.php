18a54da1d8e712df21a531f8aba3978d3f279a19
Amol Patil <73217604+amolpatilofficial@users.noreply.github.com>
2025-02-12 00:35:12 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,12 @@ $servername = 'localhost';
 $username = 'cmjzmxbk_Amolpatilofficial_Admin';
 $password = 'Shiva@88988898';
 $dbname = 'cmjzmxbk_digitalvyapari_DB';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
