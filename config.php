b3b8f6493c268de19a7aec4a148eaa87402a7a96
Amol Patil <73217604+amolpatilofficial@users.noreply.github.com>
2025-02-12 00:19:59 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
 // Database configuration
-$db_config = [
-    'servername' => 'localhost',
-    'username' => 'cmjzmxbk_Amolpatilofficial_Admin',
-    'password' => 'Shiva@88988898',
-    'dbname' => 'cmjzmxbk_digitalvyapari_DB',
-];
+$servername = 'localhost';
+$username = 'cmjzmxbk_Amolpatilofficial_Admin';
+$password = 'Shiva@88988898';
+$dbname = 'cmjzmxbk_digitalvyapari_DB';
 ?>
